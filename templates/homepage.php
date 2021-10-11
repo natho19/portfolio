3550fae216fd8edd4e7302c42ed24ca0724b760d
Nathan Akpawu <nakpawu@gmail.com>
2021-10-11 17:28:39 +0000

    Homepage frontend

--- templates/homepage.php
+++ templates/homepage.php
@@ -11,11 +11,11 @@
     <ul class='slides'>
         <li class='slide dark'>
             <section class='jumbotron'>
-                <div class='bg' style='background:url(<?= TR_IMG_URL . '/projects/test.png' ?>);'></div>
+                <div class='bg' style='background:url(<?= TR_IMG_URL . '/background.jpg' ?>);'></div>
                 <section>
                     <div class='row'>
-                        <div class='col-md-6'>
-                        <p>Hello, Je suis Nathan Akpawu. <br>Développeur web freelance.</p>
+                        <div class='col-md-12'>
+                        <h1>Hello, Je suis Nathan Akpawu, <br>Développeur Web.</h1>
                         </div>
                     </div>
                 </section>
@@ -39,7 +39,7 @@
 </section>
 
 <!-- Mes compétences -->
-<section class='contact top-line mobile-center'>
+<section class='contact wide gray mobile-center'>
     <div class='block text-center'>
         <div class="title-block">
             <h2>Mes compétences</h2>
@@ -77,117 +77,63 @@
 </section>
 
 <!-- Projets -->
-<section class='work' id='work'>
+<section class='block'>
     <div class='row'>
-        <div class='col-md-8'>
-            <div class='pull-left' id='filter-trigger'>Filter</div>
-            <ul id='filters'>
-                <li>
-                    <button data-filter='*'>All</button>
-                </li>
-                <li>
-                    <button data-filter='.design'>Design</button>
-                </li>
-                <li>
-                    <button data-filter='.photography'>Photography</button>
-                </li>
-                <li>
-                    <button data-filter='.illustration'>Illustration</button>
-                </li>
-                <li>
-                    <button data-filter='.identity'>Identity</button>
-                </li>
-            </ul>
+        <div class='col-md-12'>
+            <div class='title-block text-center'>
+                <h2>Derniers projets</h2>
+            </div>
         </div>
     </div>
-    <ul class='col-3' id='grid'>
-        <li class='item photography illustration'>
-            <a class='inner' href='single.html' style='background:url(<?= TR_IMG_URL . '/projects/17.jpg' ?>);'>
-                <div class='caption'>
-                    <div class='field'>
-                        Identity + Design
-                    </div>
-                    <div class='titles'>
-                        <div class='title'>Enhanced Drilling</div>
-                        <div class='subtitle'>Businesscards</div>
-                    </div>
-                </div>
-            </a>
-        </li>
-        <li class='item photography wide'>
-            <a class='inner' href='single.html' style='background:url(<?= TR_IMG_URL . '/projects/05.png' ?>);'>
+    <ul class='col-2' id='grid'>
+        <li class='item'>
+            <a class='inner' href='single.html' style='background:url(https://ununsplash.imgix.net/photo-1414637104192-f9ab9a0ee249?fit=crop&amp;fm=jpg&amp;h=700&amp;q=75&amp;w=1050);'>
                 <div class='caption'>
                     <div class='field'>
                         Design
                     </div>
                     <div class='titles'>
-                        <div class='title'>Badaboom</div>
-                        <div class='subtitle'>Packages</div>
+                        <div class='title'>Books</div>
+                        <div class='subtitle'>The branding</div>
                     </div>
                 </div>
             </a>
         </li>
-        <li class='item identity'>
-            <a class='inner' href='single.html' style='background:url(https://ununsplash.imgix.net/photo-1414637104192-f9ab9a0ee249?fit=crop&amp;fm=jpg&amp;h=700&amp;q=75&amp;w=1050);'>
-                <div class='caption'>
-                    <div class='field'>
-                        Typography + Branding
-                    </div>
-                    <div class='titles'>
-                        <div class='title'>Tilli</div>
-                        <div class='subtitle'>Branding</div>
-                    </div>
-                </div>
-            </a>
-        </li>
-        <li class='item design'>
+        <li class='item'>
             <a class='inner' href='single.html' style='background:url(https://unsplash.imgix.net/photo-1423145406370-2b342ae5b597?fit=crop&amp;fm=jpg&amp;h=700&amp;q=75&amp;w=1050);'>
                 <div class='caption'>
                     <div class='field'>
-                        Design + Print
+                        Photography
                     </div>
                     <div class='titles'>
-                        <div class='title'>The Magazine</div>
-                        <div class='subtitle'>Issue #01</div>
-                    </div>
-                </div>
-            </a>
-        </li>
-        <li class='item high design identity'>
-            <a class='inner' href='single.html' style='background:url(<?= TR_IMG_URL . '/projects/02.jpg' ?>);'>
-                <div class='caption'>
-                    <div class='field'>
-                        Design + Identity
-                    </div>
-                    <div class='titles'>
-                        <div class='title'>Badaboom</div>
+                        <div class='title'>Suitable</div>
                         <div class='subtitle'>Clothing</div>
                     </div>
                 </div>
             </a>
         </li>
-        <li class='item design identity'>
-            <a class='inner' href='single.html' style='background:url(https://unsplash.imgix.net/photo-1423109437485-813895ed077f?fit=crop&amp;fm=jpg&amp;h=700&amp;q=75&amp;w=1050);'>
+        <li class='item'>
+            <a class='inner' href='single.html' style='background:url(https://unsplash.imgix.net/photo-1418479631014-8cbf89db3431?fit=crop&amp;fm=jpg&amp;h=700&amp;q=75&amp;w=1050);'>
                 <div class='caption'>
                     <div class='field'>
                         Design + Identity
                     </div>
                     <div class='titles'>
-                        <div class='title'>Touromi</div>
-                        <div class='subtitle'>Briefpaper</div>
+                        <div class='title'>Yellow Mellow</div>
+                        <div class='subtitle'>Brand identity</div>
                     </div>
                 </div>
             </a>
         </li>
-        <li class='item design illustration'>
-            <a class='inner' href='single.html' style='background:url(https://unsplash.imgix.net/photo-1416400453940-65c69d70ad91?fit=crop&amp;fm=jpg&amp;h=700&amp;q=75&amp;w=1050);'>
+        <li class='item'>
+            <a class='inner' href='single.html' style='background:url(https://unsplash.imgix.net/photo-1423145406370-2b342ae5b597?fit=crop&amp;fm=jpg&amp;h=700&amp;q=75&amp;w=1050);'>
                 <div class='caption'>
                     <div class='field'>
-                        Design + Illustration
+                        Photography
                     </div>
                     <div class='titles'>
-                        <div class='title'>Touromi</div>
-                        <div class='subtitle'>Interaction Design</div>
+                        <div class='title'>Suitable</div>
+                        <div class='subtitle'>Clothing</div>
                     </div>
                 </div>
             </a>
