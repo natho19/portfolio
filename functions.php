16b035dfe2606bcf8665c2ba6ef2e096e7ab866b
Nathan Akpawu <nakpawu@gmail.com>
2021-12-13 16:58:35 +0000

    acf fields generate

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 
 require(dirname(__FILE__)) . '/inc/enqueue.php';
-require(dirname(__FILE__)) . '/inc/functions.php';
+require(dirname(__FILE__)) . '/inc/support.php';
 require(dirname(__FILE__)) . '/inc/acf.php';
+require(dirname(__FILE__)) . '/inc/acf-fields.php';
 require(dirname(__FILE__)) . '/inc/third-party/pagination.php';
